3085b6d9b481aae3f151fbef8b155cd12db51f3d
amaanraaj <amandwivedi00@gmail.com>
2023-12-24 01:29:03 +0530

    added some values in seed folder using db seeding

---
+++ src/seeders/20231223194453-add-airplanes.js
@@ -0,0 +1,43 @@
+'use strict';
+const {Op } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   // examples
+    await queryInterface.bulkInsert('Airplanes', [
+      {
+        modelNumber: 'airbus789',
+        capacity: 199,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        modelNumber: 'boeing768',
+        capacity: 230,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Airplanes', {[Op.or]:[{modelNumber:"airbus789"},{modelNumber:"boeing768"}]})
+  }
+};
