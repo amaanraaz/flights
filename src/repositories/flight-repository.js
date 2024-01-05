8342b664d6143d6c0e19f341b274d82a6be680b7
amaanraaj <amandwivedi00@gmail.com>
2024-01-05 13:33:09 +0530

    Added join between airplane, flight, and airport table

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,5 +1,6 @@
 const CrudRepository = require('./crud-repository');
-const { Flight } = require('../models')
+const { Flight,Airport,Airplane } = require('../models');
+const { Sequelize } = require('sequelize');
 
 class FlightRepository extends CrudRepository {
     constructor(){
@@ -8,7 +9,29 @@ class FlightRepository extends CrudRepository {
     async getAllFlights(filter,sort){
         const response = Flight.findAll({
             where: filter,
-            order: sort
+            order: sort,
+            include: [
+                {
+                    model: Airplane,
+                    required: true,
+                    as: 'airplane_details'
+                },
+                {
+                    model: Airport,
+                    on: {
+                        col1 : Sequelize.where(Sequelize.col('Flight.departureAirportId'), '=', Sequelize.col('departure_airport.code'))
+                    },
+                    as: 'departure_airport'
+                },
+                {
+                    model: Airport,
+                    on: {
+                        col1 : Sequelize.where(Sequelize.col('Flight.arrivalAirportId'), '=', Sequelize.col('arrival_airport.code'))
+                    },
+                    as: 'arrival_airport'
+                }
+                
+        ]
         });
         return response;
     }
