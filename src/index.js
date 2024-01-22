ab6f1580c15cbb80d649029480e9a3608bbe1d08
amaanraaj <amandwivedi00@gmail.com>
2024-01-23 00:50:44 +0530

    allow route /flightService/api to apiRoutes as /flightService is comming from api gateway proxy

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/api',apiRoutes);
+app.use('/flightService/api',apiRoutes);
 
 app.listen(ServerConfig.PORT,async() => {
     console.log("Server is up and running");
